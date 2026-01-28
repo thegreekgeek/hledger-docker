8c87972ee79963b04d8082de131f699da9399358
thegreekgeek <thegreekgeekster@gmail.com>
2026-01-28 15:31:39 -0600

    fix: expose port 5001 in run.sh helper script

--- run.sh
+++ run.sh
@@ -24,5 +24,6 @@ docker container run --rm -it --volume "$dir:/data" \
        --env HLEDGER_JOURNAL_FILE=/data/$file \
        --env LEDGER_FILE=/data/$file \
        -p 5000:5000 \
+       -p 5001:5001 \
        --user $(id --user) \
        dastapov/hledger $extra_args "$@"
